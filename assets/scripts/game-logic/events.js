6a6b70c166cf4e863fbaeeaea2ae9896d8331032
elueder <lizzie.lueder@gmail.com>
2018-06-08 16:44:24 -0400

    Work on pushing to array
    - faulty game logic right now
    
    TODO:
    - push X's and O's to empty array
    - push X's and O's to correct spot in array based on square
    - push index # of X to one array, O to another
    - cycle through wins array to check for match between array of X index #s and O index #s

--- assets/scripts/game-logic/events.js
+++ assets/scripts/game-logic/events.js
@@ -1,27 +1,14 @@
 'use strict'
 
-let turn = 0
-let text
-
-const alternate = function (cellId) {
-  if (turn === 0) {
-    cellId.text('X')
-    turn = 1
-  } else if (turn === 1) {
-    cellId.text('O')
-    turn = 0
-  }
-}
-
-const onButtonClick = function () {
-  event.preventDefault()
-  if (player === 'x') {
-    $(this).text('X')
-  } else if (player === 'o') {
-    $(this).text('O')
-  }
+const game = []
+// Push to array
+const addToArray = function (event) {
+  $(event.target).push(game)
+  console.log('event.target in events is ', event.target)
+  console.log('game in events is ', game)
+  return game
 }
 
 module.exports = {
-  onButtonClick
+  addToArray
 }
--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,7 +1,9 @@
 'use strict'
 
-const player_x = 'X'
-const player_o = 'O'
+const gameEvents = require('./game-logic/events')
+
+const playerX = 'X'
+const playerO = 'O'
 
 let game = [
   '', '', '',
@@ -9,16 +11,16 @@ let game = [
   '', '', ''
 ]
 
-// const wins = {
-//   [0, 1, 2],
-//   [3, 4, 5],
-//   [6, 7, 8],
-//   [0, 3, 6],
-//   [1, 4, 7],
-//   [2, 5, 8],
-//   [0, 4, 8],
-//   [2, 4, 6]
-// }
+const wins = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 let turn = 0
 const switchLetter = function (event) {
@@ -33,6 +35,7 @@ const switchLetter = function (event) {
   turn += 1
   console.log('event.target is ', event.target)
   console.log('turn number is ', turn)
+  gameEvents.addToArray
   return turn
 }
 
